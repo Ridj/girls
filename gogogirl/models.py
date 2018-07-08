60bf399551c8a00ee0d3108d343367ceb8dbf76f
kato <katobasiro@gmail.com>
2018-07-08 23:21:27 +0600

    pretty admin view for girls list

--- gogogirl/models.py
+++ gogogirl/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Girl(models.Model):
     girl_name = models.CharField(max_length=30)
     girl_age = models.IntegerField(default=18)
+
+    def __str__(self):
+        return self.girl_name + ', ' + str(self.girl_age)
