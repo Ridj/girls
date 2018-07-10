a2f4a92978063036be26b39960cb85eba7e84a5f
kato <katobasiro@gmail.com>
2018-07-11 03:14:17 +0600

    pretty template

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -5,18 +5,23 @@ let girlsVue = new Vue({
     data:  {
        girlsList: []
     },
-    template: `<div><ul><li v-for="girl in girlsList">{{girl.name}}, {{girl.age}}</li></ul>
-    <button @click="updateGirlsList">Update</button></div>`,
+    template: `
+    <div>
+      <ul>
+        <li v-for="girl in girlsList">{{girl.name}}, {{girl.age}}</li>
+      </ul>
+      <button @click="updateGirlsList">Update</button>
+    </div>`,
     methods: {
         updateGirlsList: function() {
             let request = new XMLHttpRequest();
             request.open('GET', '/api/girls', true);
-            request.send(null);
-            request.onreadystatechange = (function() {
+            request.onreadystatechange = () => {
                 if (request.readyState === 4 && request.status === 200) {
                     this.girlsList = JSON.parse(request.responseText);
                 }
-            }).bind(this);
+            };
+            request.send();
        }
     },
     created: function() {
