0e9324e6d21898bebea6a3d038d5d4bb9ccb0398
kato <katobasiro@gmail.com>
2018-07-16 17:35:02 +0600

    Vuex Store version

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,30 +1,58 @@
 'use strict';
 
-let girlsVue = new Vue({
+
+const store = new Vuex.Store({
+  state: {
+    girlsList: []
+  },
+  mutations: {
+    updateGirlsList: (state, payload) => state.girlsList = payload.girls,
+  },
+  actions: {
+    updateGirls({commit}) {
+      let request = new XMLHttpRequest();
+        request.open('GET', '/api/girls', true);
+        request.onreadystatechange = () => {
+            if (request.readyState === 4 && request.status === 200) {
+              commit({
+                type: 'updateGirlsList',
+                girls: JSON.parse(request.responseText)
+              });
+            }
+        };
+        request.send();
+    }
+  }
+});
+
+
+const girlsList = {
+  template: `
+  <ul>
+    <li v-for="girl in girlsList">{{girl.name}}, {{girl.age}}</li>
+  </ul>
+  `,
+  computed: Vuex.mapState({
+      girlsList: state => state.girlsList
+  })
+};
+
+
+const girlsVue = new Vue({
     el: '#app',
-    data:  {
-       girlsList: []
+    store,
+    components: {
+      girlsList: girlsList
     },
     template: `
-    <div>
-      <ul>
-        <li v-for="girl in girlsList">{{girl.name}}, {{girl.age}}</li>
-      </ul>
-      <button @click="updateGirlsList">Update</button>
-    </div>`,
-    methods: {
-        updateGirlsList: function() {
-            let request = new XMLHttpRequest();
-            request.open('GET', '/api/girls', true);
-            request.onreadystatechange = () => {
-                if (request.readyState === 4 && request.status === 200) {
-                    this.girlsList = JSON.parse(request.responseText);
-                }
-            };
-            request.send();
-       }
-    },
+      <div>
+        <girls-list></girls-list>
+        <button @click="update">Update</button>
+      </div>`,
+    methods: Vuex.mapActions({
+        update: 'updateGirls'
+    }),
     created: function() {
-        this.updateGirlsList();
+        this.update();
     }
 });
